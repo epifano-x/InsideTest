86c359b2903fa4449d04a053c474ef6276e5d31c
BRUNO ROGLIN <49683237+epifano-x@users.noreply.github.com>
2025-05-28 09:10:27 -0300

    feat implementado persisitencia in memory

--- InsideTest/Program.cs
+++ InsideTest/Program.cs
@@ -1,25 +1,32 @@
+using InsideTest.Application;
+using InsideTest.Domain.Interfaces;
+using InsideTest.Infrastructure.Data;
+using InsideTest.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Adiciona o contexto do banco de dados InMemory
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("LojaDB"));
 
+// Injeção de dependência
+builder.Services.AddScoped<IPedidoRepository, PedidoRepository>();
+builder.Services.AddScoped<IPedidoService, PedidoService>();
+
+// Controllers e Swagger
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Middleware
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
